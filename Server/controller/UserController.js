c039e8cd955736f420fe9831f389a53ae8c172fd
Yasminadouani09 <jassiadouani09@gmail.com>
2024-10-20 20:34:11 +0100

    error fixed

--- Server/controller/UserController.js
+++ Server/controller/UserController.js
@@ -1,6 +1,6 @@
 
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/Usermodel");
 const bcrypt = require("bcryptjs");
 const JWT_SECRET = process.env.JWT_SECRET
 
